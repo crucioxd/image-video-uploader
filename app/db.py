bd2452760bf660be89c91f3cad45c9f24e2ded27
Puneet Upadhayay <puneetupx@gmail.com>
2025-11-24 12:51:06 +0530

    db: relationship model between users and posts established

--- app/db.py
+++ app/db.py
@@ -6,8 +6,10 @@ from sqlalchemy import Uuid
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, relationship
-
+from fastapi import Depends
 import datetime
+
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
@@ -16,16 +18,23 @@ class Base(DeclarativeBase):
     pass
 
 
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship("Post", back_populates="user")
+
+
 class Post(Base):
     __tablename__ = "posts"
 
     # each post will have a unique id, which is generated using uuid4
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(Uuid(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text, nullable=False)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    user = relationship("User", back_populates="posts")
 
 
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -40,3 +49,7 @@ async def create_db_and_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
