ac2344d0d8cf51ba055add48ec75f3d3cf90b17d
Puneet Upadhayay <puneetupx@gmail.com>
2025-11-24 13:16:54 +0530

    Integrate FastAPI-Users routers for auth, registration, password reset, verification, and user

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, File, UploadFile, Form
-from app.schemas import PostSchema
+from app.schemas import PostSchema, UserRead, UserCreate, UserUpdate
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
@@ -10,6 +10,7 @@ import shutil
 import os
 import uuid
 import tempfile
+from app.users import current_active_user, fastapi_users, auth_backend
 
 
 @asynccontextmanager
@@ -18,6 +19,18 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+app.include_router(fastapi_users.get_auth_router(
+    auth_backend), prefix="/auth/jwt", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(),
+                   prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(),
+                   prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(),
+                   prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(
+    UserRead, UserCreate, UserUpdate), prefix="/users", tags=["users"])
 
 
 @app.post("/upload")
